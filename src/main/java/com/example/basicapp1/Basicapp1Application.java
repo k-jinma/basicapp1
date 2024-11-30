47b485b6037f50c5940df491c1346ea2eb363da8
kazuuud <jinmakazuhiro@outlook.jp>
2024-11-30 14:07:07 +0900

    first commit

---
+++ src/main/java/com/example/basicapp1/Basicapp1Application.java
@@ -0,0 +1,13 @@
+package com.example.basicapp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Basicapp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Basicapp1Application.class, args);
+	}
+
+}
